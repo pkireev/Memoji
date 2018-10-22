e31c80cdc8fa8fce0fed229778a851303a387f8e
nodejsmode <44375868+nodejsmode@users.noreply.github.com>
2018-10-22 22:11:30 +0300

    v8 FINAL
    All done! My memoji game is the best ever!

--- memoji.js
+++ memoji.js
@@ -25,6 +25,7 @@ function Card(img, orderNumber) {
 
 
 
+
     this.rotate = function () { //реализует переворот карточки как на лицо, так и на рубашку
 
         if (this.flip === undefined) {
@@ -43,28 +44,51 @@ function Card(img, orderNumber) {
 
     this.setCorrect = function () { // делаем лицевую сторону зеленой
         this.domFront.classList.add('correct');
+        this.domCard.classList.add('correct');
     };
 
     this.setIncorrect = function () { // делаем лицевую сторону красной
         this.domFront.classList.add('incorrect');
+        this.domCard.classList.add('incorrect');
     };
 
     this.removeIncorrect = function () { // снимаем красноту
         this.domFront.classList.remove('incorrect');
+        this.domCard.classList.remove('incorrect');
+
     };
+
+    this.removeAll = function () { // удаляем все классы с карты
+        this.domCard.classList.remove('rotate-card');
+        this.domCard.classList.remove('rotate-reverse');
+        this.domCard.classList.remove('correct');
+        this.domCard.classList.remove('incorrect');
+
+        this.domFront.classList.remove('opened');
+        this.domFront.classList.remove('correct');
+        this.domFront.classList.remove('incorrect');
+    };
+
+
+    // при создании карты очищаем ее прошлые состояния:
+    this.removeAll();
+
 
 }
 
 
 
 
-
-// Делаем конструктор класса игрового поля
 /*
+Делаем конструктор класса игрового поля
+
 
 в нем создаем массив объектов всех 12 карточек (изображений)
 перемешиваем массив
 создаем массив объектов карточек
+
+плюс к этому куча разных массивов - открытых карт, угаданных карт, неугаданных карт
+и таймер!
 
 
  */
@@ -98,13 +122,54 @@ function Playground(imgPath, pictures) {
     this.cardsGuessed = []; // массив из угаданных карт, которые не крутятся больше
     this.cardsRed = []; // массив из красных карт, чтобы их закрыть
 
+
+    this.timeLeft = 60; // сколько секунд на игру
+
+
+
+    this.timerId = undefined; // заготовочка под timerId
+
+
+    this.displayTimer = function () {
+        if (this.timeLeft >= 0) {
+            var mins = Math.floor(this.timeLeft / 60);
+            var secs = this.timeLeft - mins * 60;
+
+            if (mins < 10) {
+                mins = '0' + mins
+            }
+            if (secs < 10) {
+                secs = '0' + secs
+            }
+        } else {
+
+            mins = '00';
+            secs = '00';
+
+
+            document.querySelector('.form .text span:nth-child(1)').innerHTML = 'L';
+            document.querySelector('.form .text span:nth-child(2)').innerHTML = 'o';
+            document.querySelector('.form .text span:nth-child(3)').innerHTML = 's';
+            document.querySelector('.form .text span:nth-child(4)').innerHTML = 'e';
+
+
+
+            document.querySelector('.modal').classList.add('show-modal');
+            document.querySelector('.form-wrapper').classList.add('show-form');
+
+            clearInterval(this.timerId);
+        }
+
+        document.querySelector('.timer').innerHTML = mins + ":" + secs;
+    };
 }
 
 
 
 
 
-// при загрузке создаем экземпляр класса игрового поля, создаем 12 объекутов карточек со своими картинками
+
+// при загрузке создаем экземпляр класса игрового поля, создаем 12 объектов карточек со своими картинками
 
 var _imgPath = 'https://cdn.shopify.com/s/files/1/1061/1924/products/';
 var _pictures = 'tiger_emoji_icon_png_large.png?v=1480481019;' +
@@ -116,21 +181,12 @@ var _pictures = 'tiger_emoji_icon_png_large.png?v=1480481019;' +
 
 
 var play = new Playground(_imgPath, _pictures);
+play.displayTimer();
 
 
 
 
-
-
-
-
-
-
-// навесим события на клик на каждую карточку
-
-
-
-
+// навесим события на каждую карточку
 
 var cards = Array.from(document.querySelectorAll('.card'));
 
@@ -153,12 +209,26 @@ cards.forEach(function (card) {
             gameLogic(id); // передаем в функцию номер открытой только что карты, закрытую не передаем
         }
 
+
+        if (!play.timerId) {
+            // запускаем таймер, если сейчас был первый клик, определяем по отсутствующему текущему timerId
+
+            play.timerId = setInterval(function () {
+                play.timeLeft--;
+                play.displayTimer();
+            }, 1000);
+
+        }
+
     });
 });
 
 
 
 function gameLogic(id) {
+
+    // эта логика работает на клике. Если событие по таймеру, то оно обрабатывается не здесь!
+
 
     var card1, card2;
     // сначала проверим - есть ли красные карты, чтобы их закрыть
@@ -210,15 +280,58 @@ function gameLogic(id) {
             play.cardsRed.push(card1.id, card2.id); // запомним красные карты, чтобы закрыть потом
         }
 
-// сделаем задержку на 200 мс для вывода модального окна
-        if (play.cardsGuessed.length === 12) {
+
+// сделаем задержку на 400 мс для вывода модального окна, чтобы анимация успела отыграть
+
+        if (play.cardsGuessed.length === 12) { // если угадано 12 карт, т.е. все
+
+            clearInterval(play.timerId);
+
+            // сделаем соответствующую надпись на модальном окошке
+            document.querySelector('.form .text span:nth-child(1)').innerHTML = 'W';
+            document.querySelector('.form .text span:nth-child(2)').innerHTML = 'i';
+            document.querySelector('.form .text span:nth-child(3)').innerHTML = 'n';
+            document.querySelector('.form .text span:nth-child(4)').innerHTML = '!';
+
+
             setTimeout(function () {
                 document.querySelector('.modal').classList.add('show-modal');
                 document.querySelector('.form-wrapper').classList.add('show-form');
             }, 400);
 
+            // далее сработает событие по кнопке Play again, которое мы повесим чуть ниже
+
+      }
+    }
+}
+
+
+
+
+
+// повесим событие на кнопку Play again в модальном окошке:
+
+var buttonNewGame = document.querySelector('.form-button');
+buttonNewGame.addEventListener('click', function() {
+
+    play.cards.forEach(function (card) {
+        if (card.flip) {
+            // проверяем - открыта ли карта, чтобы закрыть, а то не все могут быть открыты, если LOSE
+            card.rotate();
         }
 
-    }
+        // card.removeAll(); // снимем все классы с карты
+    });
 
-}
+
+    document.querySelector('.modal').classList.remove('show-modal');
+    document.querySelector('.form-wrapper').classList.remove('show-form');
+
+
+
+    // НОВАЯ ИГРА через полсекунды, чтобы сработала анимация на закрытие карт!
+    setTimeout(function () {
+        play = new Playground(_imgPath, _pictures);
+        play.displayTimer(); // отобразим таймер
+    }, 500);
+});
